70ad6af2359bfc2b13a42e68e5b6ccff14929a2e
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-06-21 23:53:28 +0200

    input loop

--- client/final.py
+++ client/final.py
@@ -8,6 +8,26 @@ from grpc_reflection.v1alpha.proto_reflection_descriptor_database import ProtoRe
 from google.protobuf import descriptor_pool, message_factory, json_format
 
 
+def clrscr():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def input_loop():
+    while True:
+        try:
+            inp = input("[gRPC dyn client]>> ")
+        except (KeyboardInterrupt, EOFError):
+            return
+        if inp == "exit":
+            return
+        if inp == ":cls:":
+            clrscr()
+            inp = ""
+        yield inp
 
 if __name__ == "__main__":
-    pass
+    for inp in input_loop():
+        if inp is None or inp == "":
+            continue
