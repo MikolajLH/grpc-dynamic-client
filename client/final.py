4eedbec0e7b5bd3b4a545f398edd3a74c36ae7a1
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-06-21 23:52:20 +0200

    imports

--- client/final.py
+++ client/final.py
@@ -1,3 +1,11 @@
+from enum import Enum
+import os
+import platform
+import json
+
+import grpc
+from grpc_reflection.v1alpha.proto_reflection_descriptor_database import ProtoReflectionDescriptorDatabase
+from google.protobuf import descriptor_pool, message_factory, json_format
 
 
 
